4fd7d6b1179d7a05b9452d9b276d6a98441cb97a
Leigh Wilson <code-tap@leighwilson.xyz>
2021-09-11 21:51:03 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Powerd by AWS!'));
+app.get('/', (req, res) => res.send('Successful CI_CD Pipeline with review functionality!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
